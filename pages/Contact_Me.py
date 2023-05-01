23d59225c6c43bb381b089b56edd9c5a004b66a6
Danny Bui <Dannybui5155@gmail.com>
2023-05-01 14:15:25 -0500

    Connected backend function send email to front end

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
@@ -6,7 +7,14 @@ st.header("Contact Me")
 # ONLY WORKS WITH GMAIL
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Your message") # Allows multi-line text
+    raw_message = st.text_area("Your message") # Allows multi-line text
+    message = f"""Subject: New email from {user_email}
+    \n
+    From: {user_email} 
+    \n
+    {raw_message}
+    """
     button = st.form_submit_button("Submit") # Used just for forms
     if button:
-        print("I was pressed!")
+        send_email(message)
+        st.info("Your email was sent successfully.")
--- send_email.py
+++ send_email.py
@@ -1,25 +1,20 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "Dannybui5155@gmail.com"
-with open(".env", 'r') as file:
-    unsorted = file.read().split("=")
-    password = unsorted[1]
-    # print(password)
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "Dannybui5155@gmail.com"
+    with open(".env", 'r') as file:
+        unsorted = file.read().split("=")
+        password = unsorted[1]
+        # print(password)
+    receiver = "Dannybui5155@gmail.com"
+    context = ssl.create_default_context()
 
-receiver = "Dannybui5155@gmail.com"
-context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-message = """
-Hello, 
 
-This is an automated message to test some mad scientist software I made.
 
-Enjoy 
-"""
-
-with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
