249dcbd2b416998912a2f172f2836400d9186039
Danny Bui <Dannybui5155@gmail.com>
2023-05-01 14:37:50 -0500

    fixed issue with copying files

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from send_email import send_email
+import pandas
 
 st.header("Contact Me")
-
 
 # ONLY WORKS WITH GMAIL
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
     raw_message = st.text_area("Your message") # Allows multi-line text
-    message = f"""Subject: New email from {user_email}
+    message = f"""Subject: From {user_email}
     \n
     From: {user_email} 
     \n
