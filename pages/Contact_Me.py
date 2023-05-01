5c2801ef8283b55a67c7b6b13c9e1b5c1ad0d01a
Danny Bui <Dannybui5155@gmail.com>
2023-05-01 13:47:23 -0500

    adding sending email functionality

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.header("Contact Me")
 
+
+# ONLY WORKS WITH GMAIL
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
     message = st.text_area("Your message") # Allows multi-line text
---
+++ send_email.py
@@ -0,0 +1,25 @@
+import smtplib, ssl
+
+host = "smtp.gmail.com"
+port = 465
+
+username = "Dannybui5155@gmail.com"
+with open(".env", 'r') as file:
+    unsorted = file.read().split("=")
+    password = unsorted[1]
+    # print(password)
+
+receiver = "Dannybui5155@gmail.com"
+context = ssl.create_default_context()
+
+message = """
+Hello, 
+
+This is an automated message to test some mad scientist software I made.
+
+Enjoy 
+"""
+
+with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
+    server.login(username, password)
+    server.sendmail(username, receiver, message)
