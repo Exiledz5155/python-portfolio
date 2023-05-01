bec3b6a6d08a67f8e4e65dd0d714a8bf5fb59c64
Danny Bui <Dannybui5155@gmail.com>
2023-05-01 13:01:14 -0500

    Implemented a contact me page with simple form

---
+++ pages/Contact_Me.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.header("Contact Me")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your message") # Allows multi-line text
+    button = st.form_submit_button("Submit") # Used just for forms
+    if button:
+        print("I was pressed!")
